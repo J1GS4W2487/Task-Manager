1ee394f9a98e797cb17a65f07219dee8e2048fe6
J1GS4W2487 <jigmaths2487@gmial.com>
2021-07-23 23:57:26 +0530

    tejas

---
+++ leaderboard.js
@@ -0,0 +1,52 @@
+var top10=[
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        },
+        {
+            "name":"tejas",
+            "speed":"30wpm",
+            "date":"1:02pm 1/1/2001"
+        }
+]
