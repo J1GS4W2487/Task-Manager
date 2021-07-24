bd9cd45e11301b8983b972584dd248f8215cb3ca
J1GS4W2487 <jigmaths2487@gmial.com>
2021-07-24 13:31:02 +0530

    SUMMER

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const io = require('socket.io')(http);
 const express = require('express');
 const bodyParser = require("body-parser");
 const { match } = require('assert');
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/static"));
 
 app.use(express.static(__dirname+"/images"));
+
+const lboard = require('./leaderboard');
 
 
 app.get('/', function(req, res) {
@@ -23,10 +26,14 @@ var inmatch = [];
 var matchsocket = [];
 var matchpos = [];
 var frnd_waiting = [];
+var fmatchsocket = [];
+var fmatchpos = [];
+
+
 
 function getRandomInt() {
    min = Math.ceil(1);
-   max = Math.floor(8);
+   max = Math.floor(7);
    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -42,10 +49,19 @@ app.get('/race', function(req, res) {
    res.sendFile(__dirname+'/race.html');
 });
 
+app.get('/leader', function(req, res) {
+   res.sendFile(__dirname+'/leader.html');
+});
+
+app.get('/frnd', function(req, res) {
+   res.sendFile(__dirname+'/frnd_race.html');
+});
+
 
 //Whenever someone connects this gets executed
 io.on('connection', function(socket) {
    const n = Math.round(Math.random()*(10000));
+   const pnum = getRandomInt();
 
    socket.on('new-user-joined', (name) => {
     activeusers.push({'id':socket.id,'name':name});
@@ -70,10 +86,9 @@ io.on('connection', function(socket) {
 
 
    socket.on('check-waiting',(name)=>{
-      console.log(waiting)
       if(waiting.length >= 3){
          for(var h=0;h<=2;h++){
-            inmatch.push({'roomno':"room"+n,"name":waiting[h][0],'ranno':5});
+            inmatch.push({'roomno':"room"+n,"name":waiting[h][0],'ranno':0});
             socket.emit('match-found',inmatch);
             socket.broadcast.to(waiting[h][1]).emit('match-found',inmatch);
             if(h == 2){
@@ -148,7 +163,6 @@ io.on('connection', function(socket) {
    });
 
    socket.on('match-finsihed',(room)=>{
-      console.log(matchpos, matchsocket);
       for(var m=0;m<matchpos.length;m++){
          if(matchpos[m][0] == room){
             matchpos.splice(m,1);
@@ -157,7 +171,6 @@ io.on('connection', function(socket) {
 
       for(var m=0;m<matchsocket.length;m++){
          for(var h=0;h<=2;h++){
-            console.log(matchsocket[m][h]);
             if(matchsocket[m][h].roomno == room){
                socket.emit('match-finished',room);
                socket.broadcast.to(matchsocket[m][h].id).emit('match-finished',room);
@@ -165,7 +178,101 @@ io.on('connection', function(socket) {
          }
          matchsocket.splice(m,1);
       }
-      console.log(matchpos, matchsocket);
+   });
+
+   var change = 0;
+
+   socket.on("checklb",(data)=>{
+      lboard.forEach((element,index) => {
+         if(change == 0){
+            if(element.numspeed < data.speed){
+               lboard[index] = { "name":data.name,"speed":`${data.speed} wpm`,"date":"-","numspeed":data.speed}
+               change++;
+            }
+         }else{
+            if(element.numspeed != 0){
+               if(element.numspeed < data.speed){
+                  lboard[index] = { "name":data.name,"speed":`${data.speed} wpm`,"date":"-","numspeed":data.speed}
+                  change++;
+               }
+            }
+         }
+
+         if(change == 1){
+            lboard[index+1] = element;
+         }
+      })
+   });
+
+
+   socket.on("frnd-room",(name)=>{
+      socket.emit("frnd-alert",n);
+      frnd_waiting.push([n]);
+   });
+
+   socket.on("enter-race",(data)=>{
+      frnd_waiting.forEach(element => {
+         if(element[0] == data.num){
+            element.push({num:data.num,name:data.name,paranum:pnum,id:socket.id});
+            if(element.length == 4){
+               element.forEach(element2 => {
+                  if(element2.id){
+                     socket.emit('enter-race',element2)
+                     socket.broadcast.to(element2.id).emit('enter-race',element2);
+                  }
+               });
+            }
+         }
+      });
+
+
+   });
+
+   socket.on('frnd_racestart', (data) => {
+      frnd_waiting.forEach((element,index) => {
+         if(element[0] == data.roomno){
+            element.forEach((element2,index2) => {
+               if(element2.name == data.name){
+                  frnd_waiting[index][index2] = data;
+                  frnd_waiting[index][index2].id = socket.id;
+               }
+               if(frnd_waiting[index][index2].roomno == data.roomno){
+                  socket.emit('frnd_racestart',frnd_waiting[index]);
+                  socket.broadcast.to(frnd_waiting[index][index2].id).emit('frnd_racestart',frnd_waiting[index]);
+               }
+            });
+         }
+      });
+   });
+
+   socket.on('fscore-update',(data)=>{
+      frnd_waiting.forEach((element,index)=> {
+         if(element[0] == data.roomno){
+            element.forEach((element2,index2) => {
+               if(element2.name == data.name){
+                  frnd_waiting[index][index2] = data;
+                  if(data.place == "finished"){
+                  console.log(element,element.length)
+                  if(element.length == 4){
+                     frnd_waiting[index][index2].place = "1st place";
+                  }
+                  else if(element.length == 5){
+                     frnd_waiting[index][index2].place = "2nd place";
+                  }
+                  else if(element.length == 6){
+                     frnd_waiting[index][index2].place = "3rd place";
+                  }
+                  }if(data.place == "unfinished"){
+                     frnd_waiting[index][index2].place = "unfinished";
+                  }
+               }
+               if(frnd_waiting[index][index2].roomno == data.roomno){
+                  socket.emit('frnd_racestart',frnd_waiting[index]);
+                  socket.broadcast.to(frnd_waiting[index][index2].id).emit('frnd_racestart',frnd_waiting[index]);
+               }
+            });
+         }
+      });
    });
 });
 
